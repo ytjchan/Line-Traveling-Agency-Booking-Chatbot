c927b336efb1350130eae3b3058f622368f582f8
HKJeffer <jeffersonjefferchanjeffer159@yahoo.com.hk>
2017-10-22 06:07:05 +0800

    Realized FAQ table and Question table's senetence field should be longer

--- construct-db.sql
+++ construct-db.sql
@@ -1,3 +1,4 @@
+/* Clean up */
 drop table Tour cascade;
 drop table TourOffering cascade;
 drop table TourGuide cascade;
@@ -6,14 +7,16 @@ drop table Booking cascade;
 drop table Question cascade;
 drop table FAQ cascade;
 
+/* Create tables */
 create table Tour (TourID varchar(5) constraint check_id_length check (length(TourID)=5) primary key, TourName varchar(50) constraint not_null not null, TourDesc varchar(200), TourLength smallint constraint check_length check (TourLength>0));
 create table TourGuide (LineID varchar(40) primary key, Name varchar(50));
 create table TourOffering (TourID varchar(5) references Tour(TourID), OfferID varchar(8) constraint check_id_length check(length(OfferID)=8), Date date, TourGuideLineID varchar(40) references TourGuide(LineID) not null, Hotel varchar(50), Price smallint constraint check_price check (Price>0), MaxCapacity smallint constraint check_capacity check (MaxCapacity>0 and MaxCapacity>MinRequirement), MinRequirement smallint constraint check_requirement check (MinRequirement>0), Confirmed boolean, primary key(TourID, OfferID));
 create table Booker (LineID varchar(40) primary key, Name varchar(50), HKID varchar(7), PhoneNo integer, Age smallint constraint check_age check (Age>0));
-create table Booking (BookingID varchar(20) primary key, LineID varchar(40) references Booker(LineID) not null, TourID varchar(5) not null, OfferID varchar(8) not null, Adults smallint, Children smallint, Toddlers smallint, AmountPaid real, SpecialRequest varchar(100), Cancelled boolean default false, foreign key (TourID, OfferID) references TourOffering(TourID, OfferID));
-create table Question (QID integer primary key, LineID varchar(40) references Booker(LineID) not null, FullQuestion varchar(100) not null, LastFiveQuestion varchar(500), Resolved boolean default false);
-create table FAQ (Keyword varchar(50) primary key, Question varchar(100), Answer varchar(100) not null);
+create table Booking (BookingID varchar(20) primary key, LineID varchar(40) references Booker(LineID) not null, TourID varchar(5) not null, OfferID varchar(8) not null, Adults smallint, Children smallint, Toddlers smallint, AmountPaid real, SpecialRequest varchar(500), Cancelled boolean default false, foreign key (TourID, OfferID) references TourOffering(TourID, OfferID));
+create table Question (QID integer primary key, LineID varchar(40) references Booker(LineID) not null, FullQuestion varchar(200) not null, LastFiveQuestion varchar(1000), Resolved boolean default false);
+create table FAQ (Keyword varchar(200) primary key, Question varchar(200), Answer varchar(500) not null);
 
+/* Description for tables */
 comment on table Tour is 'All Tour name and description, reused by TourOffering';
 comment on table TourGuide is 'Tour guide LineID and name, reused by TourOffering';
 comment on table TourOffering is 'Tour offered on a certain day';
