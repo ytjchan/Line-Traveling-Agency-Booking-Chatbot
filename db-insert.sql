9ff326ac68f78fcc12b2f9c3b573e6debc3303d1
HKJeffer <jeffersonjefferchanjeffer159@yahoo.com.hk>
2017-10-22 06:09:31 +0800

    TODO: db-insert.sql to insert records from Excel file construct-db.sql renamed to db-construct.sql for better file searching in repos.

---
+++ db-insert.sql
@@ -0,0 +1,24 @@
+/* Tour table from "Tour List"*/
+INSERT INTO Tour (TourID,TourName,TourDesc,TourLength) VALUES ('2D001','Shimen National Forest Tour','Shimen colorful pond * stunning red maple * Staying at \"Yihua Hot Spring Hotel\"','2');
+INSERT INTO Tour (TourID,TourName,TourDesc,TourLength) VALUES ('2D002','Yangshan Hot Spring Tour','Unlimited use of hot spring * Famous Yangshan roaster cusine','2');
+INSERT INTO Tour (TourID,TourName,TourDesc,TourLength) VALUES ('2D003','Heyuan Hotspring Tour ','Five Stars Hot Spring Resort * 68 different types of hot spring * Water theme park','2');
+INSERT INTO Tour (TourID,TourName,TourDesc,TourLength) VALUES ('2D004','National Park Tour','Green Lake District (Ferry Tour Included) * Asia tallest musical fountain * Staying at 4 stars hotel ','2');
+INSERT INTO Tour (TourID,TourName,TourDesc,TourLength) VALUES ('2D005','Yummy Sight seeing Tour','Once-a-year beautiful \"Four-fins-maple\" on mountain top * BBQ chicken and piglet in a lost traditional way ','2');
+INSERT INTO Tour (TourID,TourName,TourDesc,TourLength) VALUES ('3D019','Guangdong country-side relax trip ','Baiyun Mountain * the Site of the Huangpu Military Academy * the South China Botanical Gardens.  * 4 stars hotel','3');
+INSERT INTO Tour (TourID,TourName,TourDesc,TourLength) VALUES ('3D075','Kaipinng culture tour ','Kaiping Watchtowers * Jinshan Hot Spring * 5 starts hotel','3');
+INSERT INTO Tour (TourID,TourName,TourDesc,TourLength) VALUES ('3D077','Shaoguan sight-seeing tour','Nanhua Temple * Danxia Mountain * Nanling National Forest Park','3');
+INSERT INTO Tour (TourID,TourName,TourDesc,TourLength) VALUES ('3D010','Dynamic Zhuhai-Macau tour','Jinding * Tangjiawan * Gongbei * Including one night staying at 5-stars casino resort in Macau','3');
+INSERT INTO Tour (TourID,TourName,TourDesc,TourLength) VALUES ('3D991','Qingyuan historic-landscape tour ','Baojing Palace of Yingde * Sankeng Hot Spring * Sankeng Hot Spring ','3');
+INSERT INTO Tour (TourID,TourName,TourDesc,TourLength) VALUES ('3D842','Shenzhen city tour ','Window of The World  * Splendid China & Chinese Folk Culture Village * Dafen Oil Painting Village (All tickets included)','3');
+
+/* TourGuide table from "Bookin Table" */
+
+/* TourOffering table from "Booking Table" */
+
+/* Booker table from "Customer Table" */
+
+/* Booking table from "Customer Table" */
+
+/* FAQ table from faq.md */
+
+/* Question table insertion from DB Engine */
