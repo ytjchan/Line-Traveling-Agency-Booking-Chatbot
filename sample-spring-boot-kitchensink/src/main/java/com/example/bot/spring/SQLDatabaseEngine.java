41ed0b449967ba9126b8059e44167aed8cc127a8
CHEN ZEZHOU <xmczzxy@gmail.com>
2017-10-30 04:10:36 +0000

    add some functions to make the class easier to use. Haven't tested yet.

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/SQLDatabaseEngine.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/SQLDatabaseEngine.java
@@ -11,10 +11,13 @@ import java.net.URI;
 public class SQLDatabaseEngine extends DatabaseEngine {
 	
 	
-	ResultSet search(PreparedStatement stmt) throws Exception {
-		//Write your code here
-		ResultSet rs = stmt.executeQuery();
-		return rs;
+	ResultSet query(String query) throws Exception {
+		//
+    	Connection c = this.getConnection();
+    	PreparedStatement stmt = c.prepareStatement(query); // now other classes need not to create Connection and PreparedStatement obejects
+    	ResultSet rs = stmt.executeQuery();
+    	return rs;
+
 		/*
 		Connection c = this.getConnection();
 		PreparedStatement stmt = c.prepareStatement(
@@ -44,8 +47,31 @@ public class SQLDatabaseEngine extends DatabaseEngine {
 		
 	}
 	
+	ResultSet SelectQuery(String A, String B, String C) {
+	// get query result with input A,B,C stand for "selct A from B where C"
+		String query="select " + A + " from B" + " where C";
+		return this.query(query);
+	}
 	
-	public Connection getConnection() throws URISyntaxException, SQLException {
+	ResultSet InsertQuery(String table_name, String[] values) {
+	// use query to insert into tables. String[] values must include all columns in the table e.g: insert into table_name values (values[1],values[2],...)
+		String query="insert into " + table_name + "values (";
+		for (String s: values) {
+			query+= s+",";
+		}
+		query+=")";
+		query.replace(",)", ")");
+		return this.query(query);
+		
+	}
+	
+	ResultSet UpdateQuery(String table_name, String set_values, String, condition) {
+	//e.g UPDATE table_name SET set_values WHERE condition
+		String query="update " + table_name + " Set " + set_values + " WHERE " + condition ;
+		return this.query(query);
+	}
+	
+	private Connection getConnection() throws URISyntaxException, SQLException {
 		Connection connection;
 		URI dbUri = new URI(System.getenv("DATABASE_URL"));
 
