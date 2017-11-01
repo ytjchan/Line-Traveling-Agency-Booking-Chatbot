b742a7d8f969bb5c9339ecdd430dfbc8976754be
CHEN ZEZHOU <xmczzxy@gmail.com>
2017-11-01 05:19:47 +0000

    modified KitchenSinkController to support userID

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -213,7 +213,8 @@ public class KitchenSinkController {
 		String text = content.getText();
 
         log.info("Got text message from {}: {}", replyToken, text);
-
+		
+		funInterface.setUserID(event.getSource().getUserId()); // pass userID to project interface
         funInterface.process(text);
         //now the replyType of funInterface will change depending on the text
         
