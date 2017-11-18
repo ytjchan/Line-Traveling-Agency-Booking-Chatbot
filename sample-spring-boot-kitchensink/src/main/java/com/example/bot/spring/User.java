9476ede7e031aa72a6155bc5178bcd8fd7be21a0
Albert <alberthsies@gmail.com>
2017-11-19 03:05:07 +0800

    independent pusher class 10 sec try 2

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/User.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/User.java
@@ -25,6 +25,7 @@ public class User {
 	private LinkedList<String> buffer = new LinkedList<>();
         
 	private final int TIMEOUT_TIME = 5*1000; // in milliseconds
+	//fiefefef
 	//private final int TIMEOUT_TIME = 15*60*1000; // in milliseconds
 	public final static String TIMEOUT_TEXT_MESSAGE = "*You have been in inactivity for 15mins, please restart by typing anything new*";
 	
