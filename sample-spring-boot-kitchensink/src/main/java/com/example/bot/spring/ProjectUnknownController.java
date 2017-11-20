69c8f49ad49568c65d03e695c631750d61c90540
HKJeffer <jeffersonjefferchanjeffer159@yahoo.com.hk>
2017-11-20 13:10:14 +0800

    Added javadoc for ProjectUnknownController

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/ProjectUnknownController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/ProjectUnknownController.java
@@ -1,9 +1,21 @@
 package com.example.bot.spring;
 
+/**
+ * This is the controller used to handle unknown user text messages.
+ * The message will be recorded into the database for later review.
+ * Additional info including userId and the last 5 text messages s/he sent will be recorded for tracing.
+ * And all registered staff (by saying a passcode or injefting userId into database) will be notified immediately.
+ */
 public class ProjectUnknownController {
-      
-    public ProjectUnknownController() {}
-    
+          
+    /**
+     * Method to handle unknown questions.
+     * This will record the question(String)+lsat 5 questions (String array) into the database Question table.
+     * A message will also be sent to all Staffs in Staff table.
+     * @param userID The sender if this text message.
+     * @param fullQuestion The question that the chatbot couldn't handle.
+     * @param questionArray Array of up to 5 text messages sent by the user.
+     */
     public void handleUnknown (String userID, String fullQuestion, String[] questionArray) {
             SQLDatabaseEngine dbEngine = new SQLDatabaseEngine();
 	    StringBuilder sb = new StringBuilder();
@@ -22,7 +34,11 @@ public class ProjectUnknownController {
 		    ProjectPusher.pushTextShorthand(staff, "Here is a unknown messages from user_+"+userID+" : "+fullQuestion);
 	    // above won't be executed if no staff is found
     }
-  //add a new function to get staff id form database StaffId table--cloud
+
+    /**
+     * Getter method of all Staff's userId for identifying who is a staff.
+     * @return String containing all userIds of staffs, delimited by semicolon (;)
+     */
     public String getStaffId() {
     	SQLDatabaseEngine dbEngine = new SQLDatabaseEngine();
     	return dbEngine.getStaffId();
