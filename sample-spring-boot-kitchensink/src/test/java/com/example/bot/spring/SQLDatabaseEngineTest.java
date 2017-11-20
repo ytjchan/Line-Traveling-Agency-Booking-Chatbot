6d46234099bd6ce36f31d817e9e1c4ecf3063a02
cloud <fengsutuzi@gmail.com>
2017-11-20 22:51:20 +0800

    add testwe for SQL engine for FAQ part

---
+++ sample-spring-boot-kitchensink/src/test/java/com/example/bot/spring/SQLDatabaseEngineTest.java
@@ -0,0 +1,90 @@
+package com.example.bot.spring;
+
+import com.linecorp.bot.model.message.ImageMessage;
+import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.TemplateMessage;
+import com.linecorp.bot.model.message.TextMessage;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = { SQLDatabaseEngineTest.class, SQLDatabaseEngine.class })
+public class SQLDatabaseEngineTest {
+
+	@Autowired
+	SQLDatabaseEngine db=new SQLDatabaseEngine();
+	@Test
+	/**
+	 * Test case to test for input text which is keyword, whether it can return corresponding answer
+	 */
+	public void testSearchKeywordFromFAQ1() {
+		String result=db.searchKeywordFromFAQ("vegetarian");
+		assertNotNull(result);
+		//System.out.print("\n\nhello"+result+"!!!!!!!!\n\n");
+		assertTrue(result.equals("No"));
+	}
+	@Test
+	/**
+	 * Test case to test for input text which is not keyword, whether it return empty string
+	 */
+	public void testSearchKeywordFromFAQ2() {
+		String result=db.searchKeywordFromFAQ("vegetariansaf");
+		assertNotNull(result);
+		
+		assertTrue(result.length()==0);
+	}
+	@Test
+	/**
+	 * Test case to test for duplicate input words which are keyword, whether it can return corresponding answer
+	 */
+	public void testSearchKeywordFromFAQ3() {
+		String result=db.searchKeywordFromFAQ("vegetarian vegetarian");
+		assertNotNull(result);
+		//System.out.print("\n\nhello"+result+"!!!!!!!!\n\n");
+		assertTrue(result.equals("No"));
+	}
+	@Test
+	/**
+	 * Test case to test for a input text containing one keyword and one which is not ketword, whether it can return corresponding answer
+	 * note that for input text which contain more than two words, after find one keyword,'\n\n' will be added which make reply clear for users 
+	 */
+	public void testSearchKeywordFromFAQ4() {
+		String result=db.searchKeywordFromFAQ("vegetarian vsafsa");
+		assertNotNull(result);
+		//System.out.print("\n\nhello"+result+"!!!!!!!!\n\n");
+		assertTrue(result.equals("No\n\n"));
+	}
+	
+	
+	
+	
+	
+	
+	//COMP3111FUN is all the staff id in database
+	
+	
+	@Test
+	/**
+	 * Test case to test for input text which is keyword, whether it can return corresponding answer
+	 * note that all staff id will be added a ';' which make it easier to seperate them later
+	 */
+	public void testGetStaffId() {
+		String result=db.getStaffId();
+		assertNotNull(result);
+		//System.out.print("\n\nhello"+result+"!!!!!!!!\n\n");
+		assertTrue(result.equals("COMP3111FUN;"));
+	}
+	
+}
+
